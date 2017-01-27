95b32c5911e8d47999073e67eb15bed37c80b370
Ben Keith <ben@benlk.com>
2017-01-27 17:12:27 +0000

    License statements

--- README.md
+++ README.md
@@ -5,4 +5,25 @@ Twitter avatar is https://commons.wikimedia.org/wiki/File:Calendar_Noun_project_
 ## Setup:
 
 1. Install https://github.com/sferik/t and configure it.
-2. Add `` to cron using `crontab -e`
+2. Add `( I need example command here )` to cron using `crontab -e`
+
+## License
+
+looming_midterms, this Twitter bot, is copyright 2017 and following by Ben Keith.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## Contact
+
+If you have questions about this bot, file an issue using the "Issues" tab on this GitHub page.
--- message.sh
+++ message.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-# Currently tweeting as thunderhug43210
+# Currently tweeting as looming_midterms
+# looming_midterms, this Twitter bot, is copyright 2017 and following by Ben Keith.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
 
+# Contributions to this list are welcome.
+# Empty lines in this area are for randomness.
 arr[0]='Will you run for office?'
 arr[1]=''
 arr[2]='⚑'
@@ -24,4 +40,5 @@ arr[19]='What can you do to help?'
 # random item generation from https://stackoverflow.com/questions/5189913/pick-and-print-one-of-three-strings-at-random-in-bash-script
 rand=$[ $RANDOM % ${#arr[*]} ]
 
+# Days until event from https://stackoverflow.com/questions/6282059/how-do-you-print-the-days-until-a-deadline-from-the-command-line#6282176
 echo $(expr '(' $(date -d 2018/11/6 +%s) - $(date +%s) + 86399 ')' / 86400) "days until the 2018 midterms. ""${arr[$rand]}"
