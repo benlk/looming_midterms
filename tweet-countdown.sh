02ad1bc443ef86e4fbae22b03460618c0944f9fd
Ben Keith <ben@benlk.com>
2017-01-31 15:27:14 +0000

    Fixes to the logging

--- tweet-countdown.sh
+++ tweet-countdown.sh
@@ -3,7 +3,7 @@
 # By Ben Keith, 2017
 #
 # Add this line:
-# * 9 * * * /path/to/tweet-countdown.sh > /path/to/tweet-countdown.log
+# * 9 * * * /path/to/tweet-countdown.sh >> /path/to/tweet-countdown.log 2>&1
 
 # This needs the full path to the trc, not ~, although . works.
 ./message.sh | xargs -I '{}' t update --profile=./looming_midterm.trc '{}'
