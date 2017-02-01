6e6e944d782a0561de52209782bbbc67afdf9af2
Ben Keith <ben@benlk.com>
2017-02-01 04:47:56 +0000

    Fix cron script yet again

--- tweet-countdown.sh
+++ tweet-countdown.sh
@@ -5,5 +5,6 @@
 # Add this line:
 # * 9 * * * /path/to/tweet-countdown.sh >> /path/to/tweet-countdown.log 2>&1
 
-# This needs the full path to the trc, not ~, although . works.
-./message.sh | xargs -I '{}' t update --profile=./looming_midterm.trc '{}'
+# . in the cronjob is ~, but when you run this script by yourself, it's .
+# t needs the full path to the trc, not ~, although . works.
+~/looming_midterm/message.sh | xargs -I '{}' t update --profile=./looming_midterm/looming_midterm.trc '{}'
