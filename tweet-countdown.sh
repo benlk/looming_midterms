0e6614d90f28bc25132087ee724a2d30b0620a0a
Ben Keith <ben@benlk.com>
2017-02-03 03:29:20 +0000

    Notes about crontab and path

--- tweet-countdown.sh
+++ tweet-countdown.sh
@@ -4,6 +4,10 @@
 #
 # Add this line:
 # * 9 * * * /path/to/tweet-countdown.sh >> /path/to/tweet-countdown.log 2>&1
+#
+# You will also need to run `which t` to find out the directory containing `t`
+# so that you can add it to $PATH in your crontab
+# Add this line, or one like it, when you edit your crontab with `crontab -e`
 
 # . in the cronjob is ~, but when you run this script by yourself, it's .
 # t needs the full path to the trc, not ~, although . works.
