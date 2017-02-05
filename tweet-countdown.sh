a35d91637dfc3540193c5e809f31bf317f77fab2
Ben Keith <ben@benlk.com>
2017-02-05 21:01:06 +0000

    Prevent double-posting with LOCK file

--- tweet-countdown.sh
+++ tweet-countdown.sh
@@ -6,10 +6,21 @@
 # * 9 * * * /path/to/tweet-countdown.sh >> /path/to/tweet-countdown.log 2>&1
 #
 # You will also need to run `which t` to find out the directory containing `t`
-# so that you can add it to $PATH in your crontab
-# Add this line, or one like it, when you edit your crontab with `crontab -e`
+# You will need to edit the line containing `/usr/local/bin/t` to match your system.
+
+# prevent multi-posting like what happened Feb 5: http://stackoverflow.com/a/11631222
+if [ -f /tmp/looming_midterm.lock ]
+then
+	exit
+fi
+
+touch /tmp/looming_midterm.lock
 
 # . in the cronjob is ~, but when you run this script by yourself, it's .
 # t needs the full path to the trc, not ~, although . works.
 cd ~/looming_midterm/
+
+# using /usr/local/bin/t because this isn't working otherwise
 ./message.sh | xargs -I '{}' /usr/local/bin/t update --profile=./looming_midterm.trc '{}'
+
+rm /tmp/looming_midtern.lock
