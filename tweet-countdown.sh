603b5101af805d6c16efc742620ab3c8c423621b
Ben Keith <ben@benlk.com>
2017-05-16 10:09:01 -0400

    hopeful minor change to how this works, so that we can use newlines

--- tweet-countdown.sh
+++ tweet-countdown.sh
@@ -22,6 +22,6 @@ touch /tmp/looming_midterm.lock
 cd ~/looming_midterm/
 
 # using /usr/local/bin/t because this isn't working otherwise
-./message.sh | xargs -I '{}' /usr/local/bin/t update --profile=./looming_midterm.trc '{}'
+./message.sh | xargs -I {} /usr/local/bin/t update --profile=./looming_midterm.trc '{}'
 
 rm /tmp/looming_midterm.lock
