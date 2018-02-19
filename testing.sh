d1f3acf83892f1390199cf66cd6c5341d19b643d
Ben Keith <ben@benlk.com>
2018-02-18 19:27:18 -0500

    remove testing.sh; its purpose is done

--- testing.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-output=$(echo "foo" | xargs -0 -I {} t update "{}")
-#output='Tweet posted by @looming_midterm. Run `t delete status 965374407727288320` to delete.'
-foo=$( echo $output | grep -Eo '[[:digit:]]+' )
-echo $foo
-reply_output=$( ./message.sh | xargs -0 -I {} t reply $foo "{}" )
